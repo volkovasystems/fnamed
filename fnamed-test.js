136dbb8703c1aced87ddc0592d3bf40b7fb88e36
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-08 12:08:21 +0800

    Updated test.

--- fnamed-test.js
+++ fnamed-test.js
@@ -4,8 +4,10 @@ const fnamed = require( "./fnamed.js" );
 
 assert.equal( fnamed( function( ){ } ), false, "should be false" );
 assert.equal( fnamed( ( ) => { } ), false, "should be false" );
+assert.equal( fnamed( "123" ), false, "should be false" );
+assert.equal( fnamed( "function hello( ){ }" ), false, "should return false" );
 assert.equal( fnamed( function test( ){ } ), true, "should be true" );
 assert.equal( fnamed( "hello" ), true, "should be true" );
-assert.equal( fnamed( "123" ), false, "should be false" );
+assert.equal( fnamed( function hello( ){ }, "hello" ), true, "should return true" );
 
 console.log( "ok" );
