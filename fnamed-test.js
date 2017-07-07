de7f26f69b140d2e364ea440dd0e9d22cd09a3d2
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-07 14:22:28 +0800

    Updated server/client test.

--- fnamed-test.js
+++ fnamed-test.js
@@ -2,12 +2,16 @@
 const assert = require( "assert" );
 const fnamed = require( "./fnamed.js" );
 
-assert.equal( fnamed( function( ){ } ), false, "should be false" );
-assert.equal( fnamed( ( ) => { } ), false, "should be false" );
-assert.equal( fnamed( "123" ), false, "should be false" );
-assert.equal( fnamed( "function hello( ){ }" ), false, "should return false" );
 assert.equal( fnamed( function test( ){ } ), true, "should be true" );
-assert.equal( fnamed( "hello" ), true, "should be true" );
-assert.equal( fnamed( function hello( ){ }, "hello" ), true, "should return true" );
+
+assert.equal( fnamed( Error ), true, "should be true" );
+
+assert.equal( fnamed( function( ){ } ), false, "should be false" );
+
+assert.equal( fnamed( ( ) => { } ), false, "should be false" );
+
+assert.equal( fnamed( ( entity ) => ( typeof entity == "string" ) ), false, "should be false" );
+
+assert.equal( fnamed( "function hello( ){ }" ), false, "should be false" );
 
 console.log( "ok" );
